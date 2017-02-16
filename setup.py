1ecfc1e858c7a5a9722c8beb154ef56d9a190767
Radek Goblin Pieczonka <goblin@pentex.pl>
2017-02-16 23:05:26 +0100

    pypi prep

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup
 
 setup(
-    name='Bungee',
-    version='0.1dev',
-    packages=['bungee',],
-    license='MIT',
-    scripts=['bin/bungee',]
+    name     = 'bungee',
+    packages = ['bungee',],
+    version  = '0.1',
+    description = 'templating and environment management for kubernetes',
+    license  = 'MIT',
+    scripts  = ['bin/bungee',],
+    author   = 'Radek Goblin Pieczonka',
+    author_email = 'goblin@pentex.pl',
+    url = 'https://github.com/goblain/bungee',
+    download_url = 'https://github.com/goblain/bungee/tarball/0.1',
+    keywords = ['kubernetes', 'templates', 'environment'],
+    classifiers = []
 )
